27332c168bbc4601b1e647588d95ff998409c69c
Wizard67 <wizard67.hjn@gmail.com>
2018-09-11 00:38:47 +0800

    feat: add Login view

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,43 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-    <van-button>测试</van-button>
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
+  <div class="container">
+    <van-tabs @change="onTabsChange">
+      <van-tab title="密码登录">
+        <van-cell-group>
+          <van-field
+            :value="phone"
+            placeholder="请输入手机号"
+            :border="false"
+            @change="onChange"
+          />
+          <van-field
+            :value="password"
+            type="password"
+            placeholder="请输入密码"
+            :border="false"
+            @change="onChange"
+          />
+        </van-cell-group>
+      </van-tab>
 
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+      <van-tab title="短信登录">
+        <van-cell-group>
+          <van-field
+            :value="phone"
+            placeholder="请输入手机号"
+            :border="false"
+            @change="onChange"
+          />
+          <van-field
+            :value="password"
+            type="password"
+            placeholder="请输入验证码"
+            :border="false"
+            @change="onChange"
+          />
+        </van-cell-group>
+      </van-tab>
+    </van-tabs>
+    <van-button size="large">登录</van-button>
   </div>
 </template>
 
@@ -28,8 +47,9 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {}
+      active: 0,
+      phone: '',
+      password: ''
     }
   },
 
@@ -38,68 +58,23 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
     clickHandle (msg, ev) {
       console.log('clickHandle:', msg, ev)
+    },
+    onTabsChange(event) {
+      this.active = event.mp.detail.index
+      wx.showToast({
+        title: `切换到 tab ${event.mp.detail.index}`,
+        icon: 'none'
+      });
     }
   },
 
   created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
   }
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
 </style>
