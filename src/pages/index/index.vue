bb7ac2ca76f07cd5818f7fdd720caf8249f0cdac
Wizard67 <wizard67.hjn@gmail.com>
2018-09-12 23:08:59 +0800

    refactor: index layout

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,80 @@
 <template>
   <div class="container">
-    index
+    <picker>福州</picker>
+    <swiper
+      indicator-dots
+      autoplay
+    >
+      <swiper-item>
+        <image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" class="swiper-image"/>
+      </swiper-item>
+      <swiper-item>
+        <image src="http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg" class="swiper-image"/>
+      </swiper-item>
+    </swiper>
+
+    <scroll-view scroll-y style="width: 100%;">
+      <van-card
+        title="标题"
+        desc="描述"  
+        num="2"
+        price="2.00"
+        :thumb="imageURL"
+      >
+        <view slot="footer">
+          <van-button size="mini">按钮</van-button>
+          <van-button size="mini">按钮</van-button>
+        </view>
+      </van-card>
+      <van-card
+        title="标题"
+        desc="描述"  
+        num="2"
+        price="2.00"
+        :thumb="imageURL"
+      >
+        <view slot="footer">
+          <van-button size="mini">按钮</van-button>
+          <van-button size="mini">按钮</van-button>
+        </view>
+      </van-card>
+      <van-card
+        title="标题"
+        desc="描述"  
+        num="2"
+        price="2.00"
+        :thumb="imageURL"
+      >
+        <view slot="footer">
+          <van-button size="mini">按钮</van-button>
+          <van-button size="mini">按钮</van-button>
+        </view>
+      </van-card>
+      <van-card
+        title="标题"
+        desc="描述"  
+        num="2"
+        price="2.00"
+        :thumb="imageURL"
+      >
+        <view slot="footer">
+          <van-button size="mini">按钮</van-button>
+          <van-button size="mini">按钮</van-button>
+        </view>
+      </van-card>
+      <van-card
+        title="标题"
+        desc="描述"  
+        num="2"
+        price="2.00"
+        :thumb="imageURL"
+      >
+        <view slot="footer">
+          <van-button size="mini">按钮</van-button>
+          <van-button size="mini">按钮</van-button>
+        </view>
+      </van-card>
+    </scroll-view>
   </div>
 </template>
 
@@ -10,9 +84,7 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      active: 0,
-      phone: '',
-      password: ''
+      imageURL: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
     }
   },
 
@@ -30,5 +102,10 @@ export default {
 </script>
 
 <style scoped>
-
+scroll-view {
+  height: 400px;
+}
+.swiper-image {
+  width: 100vw;
+}
 </style>
