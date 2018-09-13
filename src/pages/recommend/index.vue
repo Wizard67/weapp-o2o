fbc13aad9a3d2c59465f67bdca5fe1ac4ed78e4f
Wizard67 <wizard67.hjn@gmail.com>
2018-09-14 00:50:11 +0800

    refactor: recommend layout

--- src/pages/recommend/index.vue
+++ src/pages/recommend/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container">
-    recommend
+    <van-tabs :active="0">
+      <van-tab title="热门">
+        <scroll-view scroll-y style="width: 100%;">
+        <van-card
+          title="标题"
+          desc="描述"  
+          num="2"
+          price="2.00"
+          :thumb="imageURL"
+          v-for="i in 5"
+          :key="i"
+        >
+          <view slot="footer">
+            <van-button size="mini">按钮</van-button>
+            <van-button size="mini">按钮</van-button>
+          </view>
+        </van-card>
+        </scroll-view>
+      </van-tab>
+      <van-tab title="水果">内容 2</van-tab>
+      <van-tab title="土特产">内容 3</van-tab>
+      <van-tab title="生鲜">内容 4</van-tab>
+      <van-tab title="票务">内容 5</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -10,9 +33,7 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      active: 0,
-      phone: '',
-      password: ''
+      imageURL: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
     }
   },
 
@@ -30,5 +51,7 @@ export default {
 </script>
 
 <style scoped>
-
+scroll-view {
+  height: 450px;
+}
 </style>
