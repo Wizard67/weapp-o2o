d135e817aa2e7abbd1ccef05c4d8384ab59e8de2
Wizard67 <wizard67.hjn@gmail.com>
2018-09-15 23:52:52 +0800

    feat(Login): 完成 login 页面交互逻辑

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -4,19 +4,20 @@
       <van-tab title="密码登录">
         <van-cell-group>
           <van-field
-            :value="phone"
             placeholder="请输入手机号"
-            :border="false"
-            @change="onChange"
-            :error="false"
+            :value="normal.phone"
+            :maxlength="11"
+            :error="isNormalPhoneError"
+            @input="onNormalPhoneChange"
+            @blur="checkNormalPhoneValue"
           />
           <van-field
-            :value="password"
             type="password"
             placeholder="请输入密码"
-            :border="false"
-            @change="onChange"
-            :error="false"
+            :value="normal.password"
+            :error="isNormalPasswordError"
+            @input="onNormalPasswordChange"
+            @blur="checkNormalPasswordValue"
           />
         </van-cell-group>
       </van-tab>
@@ -24,40 +25,63 @@
       <van-tab title="短信登录">
         <van-cell-group>
           <van-field
-            :value="phone"
             placeholder="请输入手机号"
-            :border="false"
-            @change="onChange"
-            :error="false"
+            :value="message.phone"
+            :error="isMessagePhoneError"
+            @input="onMessagePhoneChange"
+            @blur="checkMessagePhoneValue"
           />
           <van-field
-            :value="password"
-            type="password"
             placeholder="请输入验证码"
-            :border="false"
-            @change="onChange"
+            :value="message.code"
+            :error="isMessageCodeError"
+            @input="onMessageCodeChange"
+            @blur="checkMessageCodeValue"
             use-button-slot
-            :error="false"
           >
-            <van-button slot="button" size="mini" type="primary">获取验证码</van-button>
+            <van-button
+             slot="button" size="mini" type="primary"
+             @click="onCodeSend"
+             :disabled="isCodeDesabled"
+            >{{ codeText }}</van-button>
           </van-field>
         </van-cell-group>
       </van-tab>
     </van-tabs>
-    <navigator url="/pages/index/main" open-type="switchTab">
-      <van-button size="large">登录</van-button>
-    </navigator>
-    <navigator url="/pages/logup/main">注册</navigator>
+    <van-button size="large" @click="onSubmit">登录</van-button>
+    <navigator class="text-helper" url="/pages/logup/main">注册</navigator>
   </div>
 </template>
 
 <script>
+const phoneRegExp = /^1(3|4|5|6|7|8|9)\d{9}$/
+const passwordRegExp = /^\S{5,}$/
+const codeRegExp = /^\S{4}$/
+
 export default {
   data () {
     return {
       active: 0,
-      phone: '',
-      password: ''
+
+      codeText: '获取验证码',
+
+      // error status
+      isNormalPhoneError: false,
+      isNormalPasswordError: false,
+      isMessagePhoneError: false,
+      isMessageCodeError: false,
+      isFormError: false,
+      isCodeDesabled: false,
+
+      // form data
+      normal: {
+        phone: '',
+        password: ''
+      },
+      message: {
+        phone: '',
+        code: ''
+      }
     }
   },
 
@@ -65,23 +89,99 @@ export default {
   },
 
   methods: {
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    setFieldStatus (result, id) {
+      if (!result) {
+        this.isFormError = true
+        this[`is${id}Error`] = true
+      } else {
+        this.isFormError = false
+        this[`is${id}Error`] = false
+      }
     },
+
+    // bind date
+    onNormalPhoneChange (e) { this.normal.phone = e.mp.detail },
+    onNormalPasswordChange (e) { this.normal.password = e.mp.detail },
+    onMessagePhoneChange (e) { this.message.phone = e.mp.detail },
+    onMessageCodeChange (e) { this.message.code = e.mp.detail },
+
+    // validate
+    checkNormalPhoneValue () {
+      this.setFieldStatus(phoneRegExp.test(this.normal.phone), 'NormalPhone')
+    },
+    checkNormalPasswordValue () {
+      this.setFieldStatus(passwordRegExp.test(this.normal.password), 'NormalPassword')
+    },
+    checkMessagePhoneValue () {
+      this.setFieldStatus(phoneRegExp.test(this.message.phone), 'MessagePhone')
+    },
+    checkMessageCodeValue () {
+      this.setFieldStatus(codeRegExp.test(this.message.code), 'MessageCode')
+    },
+
+    onCodeSend () {
+      wx.showToast({
+        title: `验证码已发送`
+      })
+      // Countdown
+      const countdown = 60
+      const rawText = this.codeText
+      this.isCodeDesabled = true
+
+      for (let index = countdown; index >= 0; index--) {
+        setTimeout(()=>{
+          this.codeText = `已发送（${index}s）`
+          if ( index === 0) {
+            this.codeText = rawText
+            this.isCodeDesabled = false
+          }
+        }, 1000 * (countdown - index))
+      }
+    },
+
+    onSubmit () {
+
+      let formDate 
+
+      if (this.active === 0) {
+        this.checkNormalPhoneValue()
+        this.checkNormalPasswordValue()
+        formDate = this.normal
+      } else {
+        this.checkMessagePhoneValue()
+        this.checkMessageCodeValue()
+        formDate = this.message
+      }
+
+      if (!this.isFormError) {
+        wx.showToast({
+          title: `发送数据 ${JSON.stringify(formDate)}`,
+          icon: 'none'
+        })
+
+        // router jump
+        setTimeout(()=>{
+          wx.switchTab({
+            url: '/pages/index/main'
+          })
+        }, 1000)
+      }
+    },
+
     onTabsChange(event) {
       this.active = event.mp.detail.index
-      wx.showToast({
-        title: `切换到 tab ${event.mp.detail.index}`,
-        icon: 'none'
-      });
+      // reset form status
+      this.isFormError = false
     }
-  },
-
-  created () {
   }
 }
 </script>
 
 <style scoped>
-
+.text-helper {
+  display: inline-block;
+  padding: 12px;
+  color: rgb(102, 102, 102);
+  font-size: 14px;
+}
 </style>
