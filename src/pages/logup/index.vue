3846e640e4cc61eaf83fccfcac5f626cd09c9af7
Wizard67 <wizard67.hjn@gmail.com>
2018-09-16 16:00:14 +0800

    feat(Logup): 完成 logup 页面交互逻辑

--- src/pages/logup/index.vue
+++ src/pages/logup/index.vue
@@ -2,47 +2,65 @@
   <div class="container">
     <van-cell-group>
       <van-field
-        :value="phone"
         placeholder="请输入手机号"
-        :border="false"
-        @change="onChange"
-        :error="false"
+        :value="form.phone"
+        :maxlength="11"
+        :error="isPhoneError"
+        @input="onPhoneChange"
+        @blur="checkPhoneValue"
       />
       <van-field
-        :value="password"
-        type="password"
         placeholder="请输入验证码"
-        :border="false"
-        @change="onChange"
+        :value="form.code"
+        :error="isCodeError"
+        @input="onCodeChange"
+        @blur="checkCodeValue"
         use-button-slot
-        :error="false"
       >
-        <van-button slot="button" size="mini" type="primary">获取验证码</van-button>
+        <van-button
+          slot="button" size="mini" type="primary"
+          :disabled="isCodeDesabled"
+          @click="onCodeSend"
+        >{{ codeText }}</van-button>
       </van-field>
 
       <van-field
-        :value="password"
         type="password"
         placeholder="请输入密码"
-        :border="false"
-        @change="onChange"
-        :error="false"
+        :value="form.password"
+        :error="isPasswordError"
+        @input="onPasswordChange"
+        @blur="checkPasswordValue"
       />
     </van-cell-group>
-    <van-button size="large">
-      <navigator url="/pages/address/main">注册</navigator>
-    </van-button>
-    <navigator url="/pages/login/main">登录</navigator>
+    <van-button size="large" @click="onSubmit">注册</van-button>
+    <navigator class="text-helper" url="/pages/login/main">登录</navigator>
   </div>
 </template>
 
 <script>
+const phoneRegExp = /^1(3|4|5|6|7|8|9)\d{9}$/
+const passwordRegExp = /^\S{5,}$/
+const codeRegExp = /^\S{4}$/
+
 export default {
   data () {
     return {
-      active: 0,
-      phone: '',
-      password: ''
+      codeText: '获取验证码',
+
+      // error status
+      isPhoneError: false,
+      isCodeError: false,
+      isPasswordError: false,
+      isFormError: false,
+      isCodeDesabled: false,
+
+      // form data
+      form: {
+        phone: '',
+        code: '',
+        password: ''
+      }
     }
   },
 
@@ -50,15 +68,70 @@ export default {
   },
 
   methods: {
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    setFieldStatus (result, id) {
+      if (!result) {
+        this.isFormError = true
+        this[`is${id}Error`] = true
+      } else {
+        this.isFormError = false
+        this[`is${id}Error`] = false
+      }
     },
-    onTabsChange(event) {
-      this.active = event.mp.detail.index
+
+    onPhoneChange (e) { this.form.phone = e.mp.detail },
+    onCodeChange (e) { this.form.code = e.mp.detail },
+    onPasswordChange (e) { this.form.password = e.mp.detail },
+
+    // validate
+    checkPhoneValue () {
+      this.setFieldStatus(phoneRegExp.test(this.form.phone), 'Phone')
+    },
+    checkCodeValue () {
+      this.setFieldStatus(codeRegExp.test(this.form.code), 'Code')
+    },
+    checkPasswordValue () {
+      this.setFieldStatus(passwordRegExp.test(this.form.password), 'Password')
+    },
+
+    onCodeSend () {
       wx.showToast({
-        title: `切换到 tab ${event.mp.detail.index}`,
-        icon: 'none'
-      });
+        title: `验证码已发送`
+      })
+      // Countdown
+      const countdown = 60
+      const rawText = this.codeText
+      this.isCodeDesabled = true
+
+      for (let index = countdown; index >= 0; index--) {
+        setTimeout(()=>{
+          this.codeText = `已发送（${index}s）`
+          if ( index === 0) {
+            this.codeText = rawText
+            this.isCodeDesabled = false
+          }
+        }, 1000 * (countdown - index))
+      }
+    },
+
+    onSubmit () {
+
+      this.checkPhoneValue()
+      this.checkCodeValue()
+      this.checkPasswordValue()
+
+      if (!this.isFormError) {
+        wx.showToast({
+          title: `发送数据 ${JSON.stringify(this.form)}`,
+          icon: 'none'
+        })
+
+        // router jump
+        setTimeout(()=>{
+          wx.redirectTo({
+            url: '/pages/login/main'
+          })
+        }, 1000)
+      }
     }
   },
 
@@ -68,5 +141,10 @@ export default {
 </script>
 
 <style scoped>
-
+.text-helper {
+  display: inline-block;
+  padding: 12px;
+  color: rgb(102, 102, 102);
+  font-size: 14px;
+}
 </style>
