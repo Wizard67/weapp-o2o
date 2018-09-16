0035f37b7f86d2535fb1a11fe4b553472f0fa5cc
Wizard67 <wizard67.hjn@gmail.com>
2018-09-16 18:44:08 +0800

    feat(Index): 完成 index 页面交互逻辑

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,77 +1,40 @@
 <template>
   <div class="container">
-    <picker>福州</picker>
+    <picker
+      id="picker"
+      class="text-helper"
+      mode="region"
+      :value="region"
+      @change="onChangeRegion"
+    >{{ region }}</picker>
     <swiper
+      id="swiper"
       indicator-dots
       autoplay
+      circular
     >
-      <swiper-item>
-        <image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" class="swiper-image"/>
-      </swiper-item>
-      <swiper-item>
-        <image src="http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg" class="swiper-image"/>
+      <swiper-item v-for="(item, key) in banners" :key="key+item.id">
+        <image :src="item.url" class="swiper-image"/>
       </swiper-item>
     </swiper>
 
-    <scroll-view scroll-y style="width: 100%;">
+    <scroll-view
+      :style="{height: scrollViewHeight+'px'}"
+      lower-threshold="50"
+      @scrolltolower="handleScrollToBottom"
+      scroll-y
+    >
       <van-card
-        title="标题"
-        desc="描述"  
+        v-for="item in lists"
+        title="原膳翔金鲳鱼340g"
+        desc="★★★★★ ￥32.50"
         num="2"
         price="2.00"
-        :thumb="imageURL"
+        :thumb="item.url"
+        :key="item"
       >
-        <view slot="footer">
-          <van-button size="mini">按钮</van-button>
-          <van-button size="mini">按钮</van-button>
-        </view>
-      </van-card>
-      <van-card
-        title="标题"
-        desc="描述"  
-        num="2"
-        price="2.00"
-        :thumb="imageURL"
-      >
-        <view slot="footer">
-          <van-button size="mini">按钮</van-button>
-          <van-button size="mini">按钮</van-button>
-        </view>
-      </van-card>
-      <van-card
-        title="标题"
-        desc="描述"  
-        num="2"
-        price="2.00"
-        :thumb="imageURL"
-      >
-        <view slot="footer">
-          <van-button size="mini">按钮</van-button>
-          <van-button size="mini">按钮</van-button>
-        </view>
-      </van-card>
-      <van-card
-        title="标题"
-        desc="描述"  
-        num="2"
-        price="2.00"
-        :thumb="imageURL"
-      >
-        <view slot="footer">
-          <van-button size="mini">按钮</van-button>
-          <van-button size="mini">按钮</van-button>
-        </view>
-      </van-card>
-      <van-card
-        title="标题"
-        desc="描述"  
-        num="2"
-        price="2.00"
-        :thumb="imageURL"
-      >
-        <view slot="footer">
-          <van-button size="mini">按钮</van-button>
-          <van-button size="mini">按钮</van-button>
+        <view class="text-helper" slot="footer">
+          仓山万达广场5.9km
         </view>
       </van-card>
     </scroll-view>
@@ -79,26 +42,88 @@
 </template>
 
 <script>
+const getPageHeight = callback => {
+  wx.getSystemInfo({success(res) {callback(res.windowHeight)}})
+}
+const getNodeRectHeight = (nodeId, callback) => {
+  wx.createSelectorQuery().select(nodeId).boundingClientRect(rect => callback(rect)).exec()
+}
+
 export default {
   data () {
     return {
-      imageURL: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
+      pageHeight: '',
+      pickerHeight: '',
+      swiperHeight: '',
+
+      region: ['福州'],
+      banners: [
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 1 },
+        { url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg', id: 2 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 3 }
+      ],
+      imageURL: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      lists: [
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 1 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 2 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 3 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 4 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 5 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 6 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 7 },
+      ]
     }
   },
 
   components: {
   },
 
+  computed: {
+    scrollViewHeight() {
+      return this.pageHeight - this.pickerHeight - this.swiperHeight
+    }
+  },
+
   methods: {
 
+    onChangeRegion (e) {
+      this.region = e.mp.detail.value[0]
+      // reload data
+    },
+
+    handleScrollToBottom (e) {
+      wx.showToast({
+        title: `加载更多...`,
+        icon: 'none'
+      })
+      this.lists.push(
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 1 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 2 },
+        { url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', id: 3 }
+      )
+    }
   },
 
   created () {
+  },
+
+  onReady () {
+    // set scroll-view height
+    getPageHeight(height => this.pageHeight = height)
+    getNodeRectHeight('#picker', rect => this.pickerHeight = rect.height)
+    getNodeRectHeight('#swiper', rect => this.swiperHeight = rect.height)
   }
 }
 </script>
 
 <style scoped>
+.text-helper {
+  display: inline-block;
+  padding: 12px;
+  color: rgb(102, 102, 102);
+  font-size: 14px;
+}
+
 scroll-view {
   height: 400px;
 }
