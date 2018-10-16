fb693fbb63eedac1b3abbd7f984a6326cb640ac2
Wizard67 <wizard67.hjn@gmail.com>
2018-10-17 00:25:08 +0800

    refactor: detail page

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="container">
     <view class="detail">
-      <image class="detail__image" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" />
-      <text class="detail__text">山东水蜜桃</text>
-      <text class="detail__text">￥26.80</text>
-      <text class="detail__text">截止时间： 2018-08-31</text>
+      <image class="detail__cover" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" />
+      <div class="detail__main">
+        <div class="detail__title">山东水蜜桃1.2kg200g以上/个山东水蜜桃1.2kg200g以上/个</div>
+        <div class="detail__summary">
+          <div class="detail__value">¥26.80<span class="detail__count">库存：1000</span></div>
+          <div class="detail__views">10000</div>
+        </div>
+        <div class="detail__helper">截止时间：2018-08-31   12:00:00</div>
+      </div>
     </view>
 
-    <van-cell-group>
-      <van-cell title="发起人：王五"/>
-    </van-cell-group>
+    <div class="panel">
+      <van-cell-group>
+        <van-cell title="发起人：王五"/>
+      </van-cell-group>
+    </div>
 
     <image class="detail__image" src="http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg" />
 
     <view class="bottomBar">
-      <text class="bottomBar__text">收藏</text>
-      <text class="bottomBar__text">分享</text>
-      <van-button type="default" @click="showCard">立即购买</van-button>
+      <van-submit-bar
+        button-text="立即购买"
+        @submit="showCard"
+      >
+        <div class="bottomBar__group">
+          <span>收藏</span>
+          <span>分享</span>
+        </div>
+      </van-submit-bar>
     </view>
 
     <van-popup
@@ -26,18 +39,24 @@
       overlay
     >
       <view class="card">
-        <image class="card__image" src="http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"/>
+        <div class="card__summary">
+          <image class="card__cover" src="http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"/>
+          <div class="card__info">
+            <div class="card__value">¥26.80</div>
+            <span class="card__count">库存：1000</span>
+          </div>
+        </div>
         <van-cell-group>
-          <van-cell title="物流方式" is-link value="快递免邮" />
           <van-cell title="购买数量">
             <slot>
               <van-stepper :value="1" @change="onStepperChange"/>
             </slot>
           </van-cell>
+          <van-cell title="物流方式" is-link value="快递免邮" />
           <van-cell value="小计 ￥200" />
         </van-cell-group>
         <navigator url="/pages/order/main">
-          <van-button type="default" block>确定</van-button>
+          <van-button type="danger" block>确定</van-button>
         </navigator>
       </view>
     </van-popup>
@@ -76,20 +95,48 @@ export default {
 </script>
 
 <style scoped>
-.text-helper {
-  display: inline-block;
-  padding: 12px;
-  color: rgb(102, 102, 102);
-  font-size: 14px;
+.detail { }
+.detail__cover {
+  width: 100%;
+  height: 674rpx;
 }
+.detail__main {
+  padding: 20rpx;
+}
+.detail__title {
+  font-size: 36rpx;
+  color: #333333;
+  line-height: 1.6;
+}
+.detail__summary {
+  display: flex;
+  justify-content: space-between;
+  margin: 20rpx 0;
+}
+.detail__value {
+  color: #FB2525;
+  font-size: 36rpx;
+}
+.detail__count {
+  margin-left: 60rpx;
+  color: #999999;
+  font-size: 24rpx;
+}
+.detail__views {
+  font-size: 24rpx;
+  color: #999999;
+}
+.detail__helper {
+  color: #424242;
+  font-size: 30rpx;
+}
+
+.panel {
+  margin: 30rpx 0;
+}
+
 .detail__image {
   width: 100%;
-}
-.detail__text {
-  display: block;
-  padding: 5px;
-  font-size: 14px;
-  color: rgb(102, 102, 102);
 }
 
 .bottomBar {
@@ -102,17 +149,33 @@ export default {
   border-top: 1px gray solid;
   background-color: white;
 }
-.bottomBar__text {
-  padding: 5px;
-  font-size: 14px;
-  color: rgb(102, 102, 102);
+.bottomBar__group {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 }
 
 .card {
   padding: 20px;
 }
-.card__image {
+.card__summary {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20rpx;
+}
+.card__cover {
   width: 100px;
   height: 100px;
+}
+.card__info {
+  padding: 20rpx;
+}
+.card__value {
+  color: #FB2525;
+  font-size: 36rpx;
+}
+.card__count {
+  color: #999999;
+  font-size: 24rpx;
 }
 </style>
