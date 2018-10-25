71cae024759ac37eabbc5c22918a5557471532f6
Wizard67 <wizard67.hjn@gmail.com>
2018-10-25 23:47:05 +0800

    refactor: user page

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -17,19 +17,19 @@
         <van-cell title="我发起的团购" value="查看更多" is-link></van-cell>
         <div class="icon-group">
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont--main icon-user-check"></div>
             <div class="icon__title">发起人认证</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont--main icon-shopping-card"></div>
             <div class="icon__title">商品管理</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont--main icon-users"></div>
             <div class="icon__title">发起团购</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont--main icon-reply"></div>
             <div class="icon__title">待评价</div>
           </div>
         </div>
@@ -41,19 +41,19 @@
         <van-cell title="我的订单" value="查看更多" is-link></van-cell>
         <div class="icon-group">
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-wallet"></div>
             <div class="icon__title">待付款</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-user-check"></div>
             <div class="icon__title">待发货</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-box"></div>
             <div class="icon__title">待收货</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-reply"></div>
             <div class="icon__title">待评价</div>
           </div>
         </div>
@@ -64,31 +64,31 @@
       <van-cell-group>
         <div class="icon-group">
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-cart-plus"></div>
             <div class="icon__title">商品收藏</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-store"></div>
             <div class="icon__title">店铺收藏</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-edit"></div>
             <div class="icon__title">我的评价</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-redo-alt"></div>
             <div class="icon__title">退款售后</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-cog"></div>
             <div class="icon__title">设置</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-marked-alt"></div>
             <div class="icon__title">收货地址</div>
           </div>
           <div class="icon">
-            <van-icon name="info-o"/>
+            <div class="iconfont icon-share-alt"></div>
             <div class="icon__title">分享</div>
           </div>
         </div>
@@ -163,6 +163,12 @@ export default {
   justify-content: center;
   width: 25%;
   padding-bottom: 20rpx;
+}
+.iconfont,
+.iconfont--main {
+  display: flex;
+  justify-content: center;
+  margin: 20rpx 0;
 }
 .icon ._van-icon {
   display: flex;
